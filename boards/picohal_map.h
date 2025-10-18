c2daeb4ffc2f3cd50a4a53f9b6ba3e13d2419655
Mitchell Grams <mitch.grams@gmail.com>
2025-10-18 12:20:17 -0600

    Update map for debugging with keypad

--- boards/picohal_map.h
+++ boards/picohal_map.h
@@ -56,40 +56,48 @@
 // Define homing/hard limit switch input pins.  Currently configured so that X and Z limit pins are shared.
 #define LIMIT_PORT            GPIO_INPUT
 #define X_LIMIT_PIN           15
-#define Y_LIMIT_PIN           10
-#define Z_LIMIT_PIN           5
+#define Y_LIMIT_PIN           15
+#define Z_LIMIT_PIN           15
 
 // Aux Outputs
 // #define AUXOUTPUT0_PORT         GPIO_OUTPUT // MODBUS DIRECTION
 // #define AUXOUTPUT0_PIN          27
-#define AUXOUTPUT1_PORT         GPIO_OUTPUT // Spindle enable
-#define AUXOUTPUT1_PIN          19
-#define AUXOUTPUT2_PORT         GPIO_OUTPUT // Spindle PWM (2 pin PWM port)
-#define AUXOUTPUT2_PIN          26
-#define AUXOUTPUT3_PORT         GPIO_OUTPUT // Spindle Direction (3 pin 'Neopixel" driver)
-#define AUXOUTPUT3_PIN          7
-#define AUXOUTPUT4_PORT         GPIO_OUTPUT // Stepper enable
-#define AUXOUTPUT4_PIN          23
-#define AUXOUTPUT5_PORT         GPIO_OUTPUT
-#define AUXOUTPUT5_PIN          20
-#define AUXOUTPUT6_PORT         GPIO_OUTPUT
-#define AUXOUTPUT6_PIN          21
-#define AUXOUTPUT7_PORT         GPIO_OUTPUT
-#define AUXOUTPUT7_PIN          22
-#define AUXOUTPUT8_PORT         GPIO_OUTPUT
-#define AUXOUTPUT8_PIN          24
+//#define AUXOUTPUT1_PORT         GPIO_OUTPUT // Spindle enable
+//#define AUXOUTPUT1_PIN          19
+// #define AUXOUTPUT2_PORT         GPIO_OUTPUT // Spindle PWM (2 pin PWM port)
+// #define AUXOUTPUT2_PIN          26
+// #define AUXOUTPUT3_PORT         GPIO_OUTPUT // Spindle Direction (3 pin 'Neopixel" driver)
+// #define AUXOUTPUT3_PIN          7
+#define AUXOUTPUT0_PORT         GPIO_OUTPUT
+#define AUXOUTPUT0_PIN          29
+#define AUXOUTPUT1_PORT         GPIO_OUTPUT
+#define AUXOUTPUT1_PIN          11
+#define AUXOUTPUT2_PORT         GPIO_OUTPUT
+#define AUXOUTPUT2_PIN          28
+#define AUXOUTPUT3_PORT         GPIO_OUTPUT
+#define AUXOUTPUT3_PIN          23
 
-#if DRIVER_SPINDLE_ENABLE
-#define SPINDLE_PORT            GPIO_OUTPUT
-#endif
 #if DRIVER_SPINDLE_ENABLE & SPINDLE_ENA
-#define SPINDLE_ENABLE_PIN      AUXOUTPUT1_PIN
+#define SPINDLE_PORT            GPIO_OUTPUT
+#define SPINDLE_ENABLE_PIN      19
+#else
+#define AUXOUTPUT4_PORT         GPIO_OUTPUT 
+#define AUXOUTPUT4_PIN          19
 #endif
-#if DRIVER_SPINDLE_ENABLE & SPINDLE_PWM
-#define SPINDLE_PWM_PIN         AUXOUTPUT2_PIN
-#endif
+
 #if DRIVER_SPINDLE_ENABLE & SPINDLE_DIR
-#define SPINDLE_DIRECTION_PIN   AUXOUTPUT3_PIN
+#define SPINDLE_DIRECTION_PIN   7
+#else
+#define AUXOUTPUT5_PORT         GPIO_OUTPUT 
+#define AUXOUTPUT5_PIN          7
+#endif
+
+#if DRIVER_SPINDLE_ENABLE & SPINDLE_PWM
+#define SPINDLE_PWM_PORT        GPIO_OUTPUT // Spindle PWM (2 pin PWM port)
+#define SPINDLE_PWM_PIN         25
+#else
+#define AUXOUTPUT0_PWM_PORT     GPIO_OUTPUT
+#define AUXOUTPUT0_PWM_PIN      25
 #endif
 
 // Define flood and mist coolant enable output pins.
@@ -97,25 +105,29 @@
 #define COOLANT_PORT            GPIO_OUTPUT
 #endif
 #if COOLANT_ENABLE & COOLANT_FLOOD
-#define COOLANT_FLOOD_PIN           25
+#define COOLANT_FLOOD_PIN           26
 #endif
 
 // Aux Inputs
 #define AUXINPUT0_PIN           6  // CNC shield HOLD pin
-#define AUXINPUT1_PIN           11 // CNC shield RUN pin
-#define AUXINPUT2_PIN           28 // CNC shield A4 (ADC capable, 3.3V max)
-#define AUXINPUT3_PIN           29 // CNC shield A5 (ADC capable, 3.3V max)
+#define AUXINPUT1_PIN           5 // CNC shield RUN pin
+#define AUXINPUT2_PIN           10 // CNC shield A4 (ADC capable, 3.3V max)
+//#define AUXINPUT3_PIN         29 // CNC shield A5 (ADC capable, 3.3V max)
 
-#undef CONTROL_ENABLE
-#define CONTROL_ENABLE 0
-
-#if PROBE_ENABLE
-#define PROBE_PIN               AUXINPUT3_PIN
-#endif
+// #undef CONTROL_ENABLE
+// #define CONTROL_ENABLE 0
 
 #if I2C_STROBE_ENABLE
-#define I2C_STROBE_PIN          AUXINPUT0_PIN   
+#define I2C_STROBE_PIN          AUXINPUT1_PIN   
 #endif
+
+#if CONTROL_ENABLE & CONTROL_HALT
+#define RESET_PIN               AUXINPUT2_PIN   
+#endif
+
+// #if PROBE_ENABLE
+// #define PROBE_PIN               AUXINPUT3_PIN
+// #endif
 
 // Modbus 
 #define MODBUS_DIR_AUX  0
